fd24bf2563b9865bf375d9386a9e59e286402abf
Belicio Batista Cardoso <belloinfo@gmail.com>
2022-12-11 11:34:57 -0300

    Criado a lista de dentista da api

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,8 +1,14 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Card from '../Components/Card';
 
 const Home = () => {
+  const [dentistas, setDentistas] = useState([]);
+
+  const apiUrl = 'https://dhodonto.ctdprojetos.com.br';
   useEffect(() => {
+    fetch(`${apiUrl}/dentista`).then((response) => {
+      response.json().then((data) => setDentistas(data));
+    });
     //Nesse useEffect, deverá ser obtido todos os dentistas da API
     //Armazena-los em um estado para posteriormente fazer um map
     //Usando o componente <Card />
@@ -12,7 +18,9 @@ const Home = () => {
     <>
       <h1>Home</h1>
       <div className='card-grid container'>
-        <Card />
+        {dentistas.map((dentista) => (
+          <Card key={dentista.matricula} dentista={dentista} />
+        ))}
       </div>
     </>
   );
