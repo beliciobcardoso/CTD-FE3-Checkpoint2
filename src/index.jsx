65beca47c963aa0dffcb9975807ca86d6cd9143f
Belicio Batista Cardoso <belloinfo@gmail.com>
2022-12-11 01:16:28 -0300

    Criado as routas

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import Navbar from "./Components/Navbar";
-import Home from "./Routes/Home";
-import Footer from "./Components/Footer";
-import "./index.css";
+import React from 'react';
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './index.css';
+import App from './App';
+import Login from './Routes/Login';
+import Home from './Routes/Home';
+import Detail from './Routes/Detail';
+import { ThemeProvider } from './hooks/useTheme';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 //Lembre-se de configurar suas rotas e seu contexto aqui
+const appRouter = createBrowserRouter([
+  {
+    path: '',
+    element: <App />,
+    children: [
+      { path: '*', loader: () => redirect('/home') },
+      { path: '', element: <Home /> },
+      { path: 'home', element: <Home /> },
+      { path: 'login', element: <Login /> },
+      { path: 'Detail', element: <Detail /> },
+    ],
+  },
+]);
 root.render(
   <React.StrictMode>
-    <Navbar />
-    <Home />
-    <Footer />
+    <ThemeProvider>
+      <RouterProvider router={appRouter} />
+    </ThemeProvider>
   </React.StrictMode>
 );
