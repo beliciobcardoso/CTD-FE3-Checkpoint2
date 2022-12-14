7ab47d8b4f1cae26605ce5cee7a6b4baedf9d44b
Belicio Batista Cardoso <belloinfo@gmail.com>
2022-12-14 10:05:46 -0300

    Finalizado o cadastro de consulta na api

--- src/Components/ScheduleForm/index.jsx
+++ src/Components/ScheduleForm/index.jsx
@@ -4,9 +4,15 @@ import styles from './ScheduleForm.module.css';
 import { apiUrl } from '../../util/urlApi';
 
 const ScheduleForm = () => {
+  const token = localStorage.getItem('authToken');
+
   const { theme } = useTheme();
   const [pacientes, setPacientes] = useState([]);
   const [dentistas, setDentistas] = useState([]);
+  const [authToken, setAuthToken] = useState('');
+  const [matriculaDentista, setMatriculaDentista] = useState('');
+  const [matriculaPaciente, setMatriculaPaciente] = useState('');
+  const [dateConsulta, setDateConsulta] = useState('');
 
   function listaDentista() {
     fetch(`${apiUrl}/dentista`).then((response) => {
@@ -22,17 +28,56 @@ const ScheduleForm = () => {
   useEffect(() => {
     //Nesse useEffect, você vai fazer um fetch na api buscando TODOS os dentistas
     //e pacientes e carregar os dados em 2 estados diferentes
+    setAuthToken(token);
     listaDentista();
     listaPaciente();
-  }, []);
+  }, [token]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    console.log(matriculaDentista);
+
     //Nesse handlesubmit você deverá usar o preventDefault,
     //obter os dados do formulário e enviá-los no corpo da requisição
     //para a rota da api que marca a consulta
     //lembre-se que essa rota precisa de um Bearer Token para funcionar.
     //Lembre-se de usar um alerta para dizer se foi bem sucedido ou ocorreu um erro
+    const consultData = {
+      paciente: {
+        matricula: matriculaPaciente,
+      },
+      dentista: {
+        matricula: matriculaDentista,
+      },
+      dataHoraAgendamento: dateConsulta,
+    };
+
+    if (authToken !== null) {
+      const requestHeaders = {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${authToken}`,
+      };
+
+      const requestConfig = {
+        method: 'POST',
+        headers: requestHeaders,
+        body: JSON.stringify(consultData),
+      };
+
+      fetch(`${apiUrl}/consulta`, requestConfig).then((response) => {
+        if (response.ok) {
+          alert('Consulta agendada com sucesso!!!');
+        } else {
+          response.text().then((dataErro) => {
+            alert(dataErro);
+          });
+        }
+      });
+    } else {
+      alert('Nescessario realizar o login');
+    }
   };
 
   return (
@@ -46,7 +91,12 @@ const ScheduleForm = () => {
               <label htmlFor='dentist' className='form-label'>
                 Dentistas
               </label>
-              <select className='form-select' name='dentist' id='dentist'>
+              <select
+                className='form-select'
+                name='dentist'
+                id='dentist'
+                onChange={(event) => setMatriculaDentista(event.target.value)}
+              >
                 {/*Aqui deve ser feito um map para listar todos os dentistas*/}
                 {dentistas.map((dentista) => (
                   <option key={dentista.matricula} value={dentista.matricula}>
@@ -59,7 +109,12 @@ const ScheduleForm = () => {
               <label htmlFor='patient' className='form-label'>
                 Pacientes
               </label>
-              <select className='form-select' name='patient' id='patient'>
+              <select
+                className='form-select'
+                name='patient'
+                id='patient'
+                onChange={(event) => setMatriculaPaciente(event.target.value)}
+              >
                 {/*Aqui deve ser feito um map para listar todos os pacientes*/}
                 {pacientes.map((paciente) => (
                   <option key={paciente.matricula} value={paciente.matricula}>
@@ -79,6 +134,7 @@ const ScheduleForm = () => {
                 id='appointmentDate'
                 name='appointmentDate'
                 type='datetime-local'
+                onChange={(e) => setDateConsulta(e.target.value)}
               />
             </div>
           </div>
@@ -86,6 +142,8 @@ const ScheduleForm = () => {
             {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar o css correto */}
             <button
+              data-bs-toggle='modal'
+              data-bs-target='#exampleModal'
               className={`btn btn-${theme} ${styles.button}`}
               type='submit'
             >
