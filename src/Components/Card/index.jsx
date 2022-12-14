33eb319f9d61f7e41b08cf323612d319bb71c7de
Belicio Batista Cardoso <belloinfo@gmail.com>
2022-12-14 08:05:35 -0300

    Ajustes de CSS

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -9,22 +9,22 @@ const Card = (props) => {
       {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar o css correto */}
       <div className={`card ${theme === 'dark' ? styles.cardDark : ''} `}>
-        <img
-          className='card-img-top'
-          src='/images/doctor.jpg'
-          alt='doctor placeholder'
-        />
-        <div className={`card-body ${styles.CardBody}`}>
-          {/* Na linha seguinte o link deverá utilizar a matricula, nome e sobrenome do dentista
+        <a href={`/Detail/${props.dentista.matricula}`}>
+          <img
+            className='card-img-top'
+            src='/images/doctor.jpg'
+            alt='doctor placeholder'
+          />
+          <div className={`card-body ${styles.CardBody}`}>
+            {/* Na linha seguinte o link deverá utilizar a matricula, nome e sobrenome do dentista
           que vem da API */}
-          {`${props.dentista.nome}  ${props.dentista.sobrenome}`}
-          <a href={`/Detail/${props.dentista.matricula}`}>
+            {`${props.dentista.nome}  ${props.dentista.sobrenome}`}
             {console.log()}
             <h5 className={`card-title ${styles.title}`}>
               <p>{props.dentista.usuario.username}</p>
             </h5>
-          </a>
-        </div>
+          </div>
+        </a>
       </div>
     </>
   );
