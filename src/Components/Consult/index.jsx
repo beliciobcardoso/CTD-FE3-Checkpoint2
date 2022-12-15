c34206049c2a22c2033fbef1169a73cfbb3544cb
Belicio Batista Cardoso <belloinfo@gmail.com>
2022-12-15 20:50:36 -0300

    estilização css concluida

--- src/Components/Consult/index.jsx
+++ src/Components/Consult/index.jsx
@@ -1,32 +1,32 @@
 import { useState, useEffect } from 'react';
 import { useTheme } from '../../hooks/useTheme';
+import { useParams } from 'react-router-dom';
 import { apiUrl } from '../../util/urlApi';
 import { convertDate } from '../../hooks/convertDate';
 import { convertTime } from '../../hooks/convertTime';
-// import styles from './consultModal.module.css';
+import styles from './consultModal.module.css';
 
 const Consult = () => {
   const { theme } = useTheme();
+  const { matricula } = useParams();
 
   const [consultas, setConsultas] = useState([]);
 
   useEffect(() => {
-    fetch(
-      `${apiUrl}/consulta?matricula=71c070b6-406b-4d16-9eed-b04a798c16eb`
-    ).then((response) => {
+    fetch(`${apiUrl}/consulta?matricula=${matricula}`).then((response) => {
       response.json().then((data) => setConsultas(data));
     });
-  }, []);
+  }, [matricula]);
 
   return (
     <>
       <div className={`${theme}`}>
-        <table>
+        <table className={`table table-striped table-${theme}`}>
           <thead>
-            <tr key=''>
-              <td>Paciente</td>
-              <td>Data</td>
-              <td>Horas</td>
+            <tr>
+              <th scope='col'>Paciente</th>
+              <th scope='col'>Data</th>
+              <th scope='col'>Horas</th>
             </tr>
           </thead>
           <tbody>
